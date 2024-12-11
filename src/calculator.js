3a0ed758b3dc45517be480080452523e6e7e22e9
marin0suke <monk.marina@gmail.com>
2024-12-11 16:42:32 +1000

    Red: calculator object needing the same validation across four methods - refactoring for repetitiveness.

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,12 @@
 export const calculator = {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
+    add: (a, b) => {
+        if (typeof a !== "number" || typeof b !== "number") throw new Error;
+        return a + b;
+    },
+    subtract: (a, b) => {
+        if (typeof a !== "number" || typeof b !== "number") throw new Error;
+        return a - b;
+    },
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
 };
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -15,3 +15,39 @@ test('subtract 5 from 20 equals 15', () => {
 test('subtract 50 from 200 equals 150', () => {
     expect(calculator.subtract(200, 50)).toEqual(150);
 })
+
+test('multiply two numbers', () => {
+    expect(calculator.multiply(20, 50)).toEqual(1000);
+})
+
+test('multiply two numbers', () => {
+    expect(calculator.multiply(5, 5)).toEqual(25);
+})
+
+test('divide left operand by right operand', () => {
+    expect(calculator.divide(100, 10)).toEqual(10);
+})
+
+test('divide left operand by right operand', () => {
+    expect(calculator.divide(44, 4)).toEqual(11);
+})
+
+test('divide left operand by right operand', () => {
+    expect(calculator.divide(0, 1)).toEqual(0);
+})
+
+test('divide left operand by right operand', () => {
+    expect(calculator.divide(-10, 2)).toEqual(-5);
+})
+
+test('throws error when input is non-numeric', () => {
+    expect(() => calculator.add(10, "five")).toThrow(Error);
+})
+
+test('throws error when input is non-numeric', () => {
+    expect(() => calculator.subtract(10, "five")).toThrow(Error);
+})
+
+test('throws error when input is non-numeric', () => {
+    expect(() => calculator.multiply(10, "five")).toThrow(Error);
+})
