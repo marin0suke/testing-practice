63a445ff55816462680d4f97086c561cfd802d5d
marin0suke <monk.marina@gmail.com>
2024-12-11 16:21:53 +1000

    Red: Add test to challenge current code.

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -11,3 +11,7 @@ test('adds two numbers', () => {
 test('subtract 5 from 20 equals 15', () => {
     expect(calculator.subtract(20, 5)).toEqual(15);
 })
+
+test('subtract 50 from 200 equals 150', () => {
+    expect(calculator.subtract(200, 50)).toEqual(150);
+})
