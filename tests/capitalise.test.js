1f7455c6e2f0d1bd97d8bd3a16aa58545f2822ab
marin0suke <monk.marina@gmail.com>
2024-12-11 14:50:35 +1000

    Step 5: Add edge cases - for empty string, already capitalised string, and single character (no function updates needed).

--- tests/capitalise.test.js
+++ tests/capitalise.test.js
@@ -7,3 +7,15 @@ test('first letter of hello is capitalised', () => {
 test('first letter of world is capitalised', () => {
     expect(capitalise("world")).toBe("World");
 })
+
+test('empty string returns an empty string', () => {
+    expect(capitalise("")).toBe("");
+})
+
+test('capitalised string will return capitalised string', () => {
+    expect(capitalise("Worm")).toBe("Worm");
+})
+
+test('single char will be capitalised', () => {
+    expect(capitalise("c")).toBe("C");
+})
